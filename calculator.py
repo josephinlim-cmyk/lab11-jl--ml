ab7a0887ce50dc784712e01edfc969262303bbe6
josephinelim-cmyk <josephinlim@ufl.edu>
2025-11-19 21:06:23 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -20,9 +20,9 @@ def add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
-def multiply(a, b):
+def mul(a, b):
     return a * b
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
@@ -30,5 +30,5 @@ def logarithm(a, b):
     if a <= 0  or b <= 0:
         raise ValueError
     return math.log(b, a)
-def exponent(a, b):
+def exp(a, b):
     return a**b
